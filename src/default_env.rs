4b4e72ae74c9c39ab732e3257414d70982624036
Martin Billinger <mbillingr@users.noreply.github.com>
2023-11-24 16:17:54 +0100

    Refactor primitives in interpreter environment
    Reorganized primitive operations are now divided into separate functions based on their data types: integers, floating points, and strings. This refactor also includes the addition of specific push methods, improving the handling and processing of different data types.

--- src/default_env.rs
+++ src/default_env.rs
@@ -7,23 +7,57 @@ pub fn default_env() -> HashMap<Symbol, Binding> {
     let mut env = HashMap::new();
     let e = &mut env;
 
+    housekeeping_primitives(e);
+    stackop_primitives(e);
+    integer_primitives(e);
+    float_primitives(e);
+    string_primitives(e);
+
+    env
+}
+
+fn housekeeping_primitives(e: &mut HashMap<Symbol, Binding>) {
     primitive(e, "%error", |intp| {
         let msg = intp.pop_str()?;
         Err(msg.to_string())
     });
+}
 
+fn stackop_primitives(e: &mut HashMap<Symbol, Binding>) {
+    primitive(e, "%drop", |intp| {
+        let _ = intp.pop();
+        Ok(())
+    });
+
+    primitive(e, "%dup", |intp| {
+        let x = intp.pop()?;
+        intp.push(x.clone());
+        intp.push(x);
+        Ok(())
+    });
+
+    primitive(e, "%swap", |intp| {
+        let b = intp.pop()?;
+        let a = intp.pop()?;
+        intp.push(b);
+        intp.push(a);
+        Ok(())
+    });
+
+    primitive(e, "%rot", |intp| {
+        let c = intp.pop()?;
+        let b = intp.pop()?;
+        let a = intp.pop()?;
+        intp.push(b);
+        intp.push(c);
+        intp.push(a);
+        Ok(())
+    });
+}
+
+fn integer_primitives(e: &mut HashMap<Symbol, Binding>) {
     primitive(e, "%i.", |intp| {
         println!("{}", intp.pop_int()?);
-        Ok(())
-    });
-
-    primitive(e, "%f.", |intp| {
-        println!("{}", intp.pop_flt()?);
-        Ok(())
-    });
-
-    primitive(e, "%s.", |intp| {
-        println!("{}", intp.pop_str()?);
         Ok(())
     });
 
@@ -34,24 +68,10 @@ pub fn default_env() -> HashMap<Symbol, Binding> {
         Ok(())
     });
 
-    primitive(e, "%ff+", |intp| {
-        let b = intp.pop_flt()?;
-        let a = intp.pop_flt()?;
-        intp.push_flt(a + b);
-        Ok(())
-    });
-
     primitive(e, "%ii-", |intp| {
         let b = intp.pop_int()?;
         let a = intp.pop_int()?;
         intp.push_int(a - b);
-        Ok(())
-    });
-
-    primitive(e, "%ff-", |intp| {
-        let b = intp.pop_flt()?;
-        let a = intp.pop_flt()?;
-        intp.push_flt(a - b);
         Ok(())
     });
 
@@ -62,17 +82,39 @@ pub fn default_env() -> HashMap<Symbol, Binding> {
         Ok(())
     });
 
-    primitive(e, "%ff*", |intp| {
-        let b = intp.pop_flt()?;
-        let a = intp.pop_flt()?;
-        intp.push_flt(a * b);
-        Ok(())
-    });
-
     primitive(e, "%ii/", |intp| {
         let b = intp.pop_int()?;
         let a = intp.pop_int()?;
         intp.push_int(a / b);
+        Ok(())
+    });
+}
+
+fn float_primitives(e: &mut HashMap<Symbol, Binding>) {
+    primitive(e, "%f.", |intp| {
+        println!("{}", intp.pop_flt()?);
+        Ok(())
+    });
+
+
+    primitive(e, "%ff+", |intp| {
+        let b = intp.pop_flt()?;
+        let a = intp.pop_flt()?;
+        intp.push_flt(a + b);
+        Ok(())
+    });
+
+    primitive(e, "%ff-", |intp| {
+        let b = intp.pop_flt()?;
+        let a = intp.pop_flt()?;
+        intp.push_flt(a - b);
+        Ok(())
+    });
+
+    primitive(e, "%ff*", |intp| {
+        let b = intp.pop_flt()?;
+        let a = intp.pop_flt()?;
+        intp.push_flt(a * b);
         Ok(())
     });
 
@@ -82,8 +124,13 @@ pub fn default_env() -> HashMap<Symbol, Binding> {
         intp.push_flt(a / b);
         Ok(())
     });
+}
 
-    env
+fn string_primitives(e: &mut HashMap<Symbol, Binding>) {
+    primitive(e, "%s.", |intp| {
+        println!("{}", intp.pop_str()?);
+        Ok(())
+    });
 }
 
 fn primitive(
